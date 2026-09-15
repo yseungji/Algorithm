d02896744c668aafa5710f25cf818238929c8fbe
유승지 <143983091+yseungji@users.noreply.github.com>
2026-09-15 14:03:23 +0900

    [level 3] Title: 입국심사, Time: 15.84 ms, Memory: 7.36 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,31 +1,40 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <iostream>
 using namespace std;
 
 long long solution(int n, vector<int> times) {
-    long long answer = 0;
-    
-    long long left = 1;
-    long long right = (long long) *max_element(times.begin(), times.end()) * n;
-
-
-    while( left <= right) {
-        long long mid = (left + right) / 2;
-        long long total = 0;
-        for(int i=0; i < times.size(); i++) {
-            total += mid / times[i];
-        }
-        if(total >= n) {
-            answer = mid;
-            right = mid - 1;
-        }
-        else {
-            left = mid + 1;
+    long long answer;
+    long long times_max=times[0];
+    for(int i=1;i<times.size();i++){
+        if(times_max < times[i]) {
+            times_max = times[i];
         }
     }
-    
-    return answer;
+    long long left = 1;
+    long long right = n * times_max;
+    cout << right << '\n';
+    long long mid;
+    //n명 이상 처리할 수 있는 최초의 시간을 찾는 알고리즘.
+    while(left < right) {
+        answer = 0;
+        mid = (left + right) / 2;
+        for(int i=0;i<times.size();i++){
+            answer += mid / times[i];
+            //cout << "left:" << left << ' ';
+            //cout << "right:" << right << ' ';
+            //cout << "mid:" << mid << ' ';
+            //cout << "answer:" << answer << '\n';
+        }
+        if(answer >= n) {
+            right = mid;
+        }
+        else if(answer < n) {
+            left = mid+1;
+        }
+    }
+    //left == right 상태.
+    return left;
 }
 
