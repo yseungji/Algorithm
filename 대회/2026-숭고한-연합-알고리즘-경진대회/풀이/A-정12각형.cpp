bd7530922b8badc2b3d9b1b727f6c76ada7a6d25
유승지 <143983091+yseungji@users.noreply.github.com>
2026-09-19 23:17:45 +0900

    Add solution for A 정12각형

---
+++ 대회/2026-숭고한-연합-알고리즘-경진대회/풀이/A-정12각형.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int T;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> T;
+
+    while (T--) {
+        int r;
+        cin >> r;
+        cout << r * r * 3 << '\n';
+    }
+
+
+    return 0;
+}
